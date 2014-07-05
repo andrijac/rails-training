345f76296d9cbf6f0e8248ac5d46d1ba5cf3e6ed
andrijac <andrija11@gmail.com>
2014-07-05 16:16:12 +0000

    numbers, strings and arrays

---
+++ 04/13_arrays.rb
@@ -0,0 +1,20 @@
+arr = [10, 20, 30, 40, 50]
+
+puts arr[-1]
+puts arr[-2]
+
+print arr
+puts "\n"
+puts '****'
+
+arr[1,2] = 22,33
+
+print arr
+puts "\n"
+
+puts '****'
+
+arr[0..2] = 1,2,3
+
+print arr
+puts "\n"
