4a54a43dd50acb4e5cfcdbdbbbe46d37b41f1794
andrijac <andrija11@gmail.com>
2014-07-05 17:20:49 +0000

    array, hash, blocks

---
+++ 04/14_hashes.rb
@@ -0,0 +1,12 @@
+
+person = {:fname => 'Brian', :lname => 'Hoke', :age => 44}
+
+puts ''
+puts '****'
+
+puts person
+
+puts person[:fname]
+
+person2 = {fname: 'Jane', lname: "Doe", age: 72}
+puts person2 
