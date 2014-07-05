acf6db548c9ba0b2c588cd9b8ff39aa686267228
andrijac <andrija11@gmail.com>
2014-07-05 08:06:19 +0000

    ruby lang

---
+++ 03/02_comments.rb
@@ -0,0 +1,11 @@
+# here is single line comment
+
+x = 3
+
+=begin
+
+mutiline comment
+
+=end
+
+puts x
---
+++ 03/04_simple-object.rb
@@ -0,0 +1,7 @@
+name = "Smith"
+name = name.upcase
+puts name
+
+name = "Smith"
+name.upcase!
+puts name
